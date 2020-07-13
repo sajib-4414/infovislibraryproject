b00790fecfafd4e13b7d30fff0ee41f7bd54a710
Shamsul Arefin <shamsul.arefin.sajib@gmail.com>
2020-07-12 21:59:34 -0600

    api call 1 method done

--- infovisproject1/library/urls.py
+++ infovisproject1/library/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('test-api-call/', views.my_django_view, name='my_django_view'),
 ]
--- infovisproject1/library/views.py
+++ infovisproject1/library/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.http import JsonResponse
+import requests
 # Create your views here.
 
 
@@ -10,3 +12,14 @@ def index(request):
     context = {}
     return render(request, template, context)
     # return HttpResponse("Hello, world. You're at the polls index.")
+
+def my_django_view(request):
+    if request.method == 'GET':
+        response = requests.get('http://openlibrary.org/subjects/romance.json?limit=5', params=request.GET)
+    # else:
+    #     r = requests.get('https://www.somedomain.com/some/url/save', params=request.GET)
+    if response.status_code == 200:
+        response2= response.json()
+        key= response2['key']
+        return HttpResponse(str(key))
+    return HttpResponse('Could not save data'+str(response.status_code))
