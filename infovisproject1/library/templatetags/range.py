bdac094c83505d9e7259b5331948fcfb7bd8b0e7
Shamsul Arefin <shamsul.arefin.sajib@gmail.com>
2020-07-19 22:30:07 -0600

    view calls successfully the search api and sets dummy response

--- infovisproject1/library/templatetags/range.py
+++ infovisproject1/library/templatetags/range.py
@@ -9,3 +9,7 @@ def filter_range(start, end):
 @register.filter
 def to_and(value):
     return value.replace("/books/","")
+
+@register.filter
+def replace_space_with_plus(value):
+    return value.replace(" ","+")
