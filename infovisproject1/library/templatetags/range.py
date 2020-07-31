53c6529cb45ceeed8db90382c10e0eb2a5ce22d2
Shamsul Arefin <shamsul.arefin.sajib@gmail.com>
2020-07-31 02:52:35 -0600

    homepage has some random data now

--- infovisproject1/library/templatetags/range.py
+++ infovisproject1/library/templatetags/range.py
@@ -1,5 +1,6 @@
 from django import template
 import re
+import random
 
 register = template.Library()
 
@@ -23,3 +24,15 @@ def replace_space_with_plus(value):
 def remove_special_characters(value):
     plain_value = re.sub("[!@#$&/.'()]", '', value)
     return plain_value
+
+@register.simple_tag
+def random_int(a, b=None):
+    if b is None:
+        a, b = 0, a
+    return random.randint(a, b)
+
+@register.simple_tag
+def random_float(a, b=None):
+    if b is None:
+        a, b = 0, a
+    return round(random.uniform(a,b), 1)
